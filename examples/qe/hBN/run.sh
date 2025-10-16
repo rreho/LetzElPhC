e37ed257c3090f2cd20ffe3db9be451f53b8bef4
Riccardo Reho <riccardoreho95@gmail.com>
2025-10-16 16:01:10 +0200

    add silicon example

---
+++ examples/qe/hBN/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+
+PW=pw.x
+PH=ph.x
+P2Y=p2y
+YAMBO=yambo
+LELPH=../../../../src/lelphc
+NCORES=1
+#run scf
+cd scf
+mpirun -np $NCORES $PW < scf.in | tee scf.out
+cd ..
+# run phonons
+cd ph
+cp -r ../scf/hBN.* .
+mpirun -np $NCORES $PH < ph.in | tee ph.out
+cd ..
+# run nscf
+cd nscf
+cp -r ../scf/hBN.* .
+mpirun -np $NCORES $PW < nscf.in | tee nscf.out
+cd hBN.save
+## create save dir
+$P2Y
+$YAMBO
+cd ../..
+######### lets start LetzElPhC 
+## first create ph_save in ph directory
+cd ph
+$LELPH -pp --code=qe -F ph.in
+cd ..
+## now compute the elph matrix elements
+cd elph
+$LELPH -F elph.in
+cd ..
