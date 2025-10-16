9b18d7493db8d42042cd84f3076dc1b5f79bff91
Riccardo Reho <riccardoreho95@gmail.com>
2025-10-16 16:17:28 +0200

    add mos2 example

---
+++ examples/qe/mos2/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+
+PW=pw.x
+PH=ph.x
+P2Y=p2y
+YAMBO=yambo
+LELPH=../../../../src/lelphc
+NCORES=8
+#run scf
+cd scf
+mpirun -np $NCORES $PW < scf.in | tee scf.out
+cd ..
+# run phonons
+cd ph
+cp -r ../scf/mos2.* .
+mpirun -np $NCORES $PH < ph.in | tee ph.out
+cd ..
+# run nscf
+cd nscf
+cp -r ../scf/mos2.* .
+mpirun -np $NCORES $PW < nscf.in | tee nscf.out
+cd mos2.save
+## create save dir
+$P2Y
+$YAMBO
+cd ../..
+######### lets start LetzElPhC 
+## first create ph_save in ph directory
+cd ph
+$LELPH -pp --code=qe -F ph.in
+cd ..
+## now compute the elph matrix elements
+cd elph
+$LELPH -F elph.in
+cd ..
