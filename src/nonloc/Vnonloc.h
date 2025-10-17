08eecef330fdea100ec53ca34134f30978621ec8
Riccardo Reho <riccardoreho95@gmail.com>
2025-10-17 16:14:10 +0200

    Document Vnonloc.h

--- src/nonloc/Vnonloc.h
+++ src/nonloc/Vnonloc.h
@@ -1,6 +1,6 @@
 /**
  * @file Vnonloc.h
- * @brief Non-local pseudopotential electron-\%phonon coupling
+ * @brief Non-local pseudopotential electron-%phonon coupling
  */
 
 #pragma once
@@ -11,7 +11,7 @@
  * @brief Sum non-local potential contribution across k and k+q points
  * @param K_ptr Projector coefficients at k
  * @param Kp_ptr Projector coefficients at k+q
- * @param fcoeff Non-local \%phonon coupling coefficient
+ * @param fcoeff Non-local %phonon coupling coefficient
  * @param nspin Number of spin states
  * @param nbnd Number of bands
  * @param nspinor Spinor components (1 or 2)
@@ -22,7 +22,7 @@ void sum_VNL_KKp(ELPH_cmplx* K_ptr, ELPH_cmplx* Kp_ptr, ELPH_cmplx* fcoeff,
                  ND_int nspin, ND_int nbnd, ND_int nspinor, ELPH_cmplx* out);
 
 /**
- * @brief Add non-local pseudopotential electron-\%phonon coupling
+ * @brief Add non-local pseudopotential electron-%phonon coupling
  * @param wfcs Wave function data
  * @param lattice Lattice structure
  * @param pseudo Pseudopotential data including projectors
@@ -30,7 +30,7 @@ void sum_VNL_KKp(ELPH_cmplx* K_ptr, ELPH_cmplx* Kp_ptr, ELPH_cmplx* fcoeff,
  * @param ik k-point index
  * @param kqsym Symmetry index for k+q
  * @param ksym Symmetry index for k
- * @param eigVec \%Phonon eigenvector
+ * @param eigVec %Phonon eigenvector
  * @param elph_kq_mn Non-local coupling matrix (in/out)
  * @param Comm MPI communicator structure
  * @return void
